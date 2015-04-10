50429ae7a66066a16cf0b4d2cf10823402a430e6
Lolirofle <lolipopple@hotmail.com>
2015-04-10 18:52:56 +0200

    Escaped string

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt,MacEager,MacResult,DummyResult};
 use syntax::ext::build::AstBuilder;
 use syntax::parse::token;
+use syntax::print::pprust;
+use syntax::fold::Folder;
 use rustc::plugin::Registry;
 
 #[plugin_registrar]
@@ -18,19 +20,11 @@ pub fn plugin_registrar(reg: &mut Registry){
 }
 
 fn expand_css<'context>(context: &'context mut ExtCtxt,span: Span,tts: &[ast::TokenTree]) -> Box<MacResult + 'context>{
-	let input = match tts{
-		[ast::TtToken(_,token::Literal(token::Lit::Str_(s),_))] => token::get_name(s),
-		[ref tt] => {
-			let span = tt.get_span();
-			context.span_err(span,"Expected a string literal");
-			return DummyResult::any(span);
-		},
-		_ => {
-			context.span_err(span,"Expected a single string literal as the arguments");
-			return DummyResult::any(span);
-		}
+	let input = match parse_single_string_literal(context,tts){
+		Some(s) => s,
+		None => return DummyResult::any(span)
 	};
-println!("{}",input);
+
 	let mut parser = css::Parser::new(&*input);
 	let mut output = String::new();
 
@@ -59,4 +53,51 @@ println!("{}",input);
 	}
 
 	MacEager::expr(context.expr_str(span,token::intern_and_get_ident(&*output)))
+}
+
+////////////////////////////////////////////////////////////////////
+// This code snippet is copied from the `rust-lang/regex` package and slightly modified.
+// (Date: 2015-04-10. Version: 0.1.27. Commit: 910aef40aca4f525dd2fecc54a78e9bc183039d1)
+// (`https://github.com/rust-lang/regex/blob/910aef40aca4f525dd2fecc54a78e9bc183039d1/regex_macros/src/lib.rs`)
+// Credits goes to the developers of the regex package.
+////////////////////////////////////////////////////////////////////
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+////////////////////////////////////////////////////////////////////
+/// Looks for a single string literal and returns it.
+/// Otherwise, logs an error with cx.span_err and returns None.
+fn parse_single_string_literal(cx: &mut ExtCtxt, tts: &[ast::TokenTree]) -> Option<String> {
+    let mut parser = cx.new_parser_from_tts(tts);
+    let entry = cx.expander().fold_expr(parser.parse_expr());
+    let regex = match entry.node {
+        ast::ExprLit(ref lit) => {
+            match lit.node {
+                ast::LitStr(ref s, _) => s.to_string(),
+                _ => {
+                    cx.span_err(entry.span, &format!(
+                        "expected string literal but got `{}`",
+                        pprust::lit_to_string(&**lit)));
+                    return None
+                }
+            }
+        }
+        _ => {
+            cx.span_err(entry.span, &format!(
+                "expected string literal but got `{}`",
+                pprust::expr_to_string(&*entry)));
+            return None
+        }
+    };
+    if !parser.eat(&token::Eof)/*.ok().unwrap()*/ {
+        cx.span_err(parser.span, "only one string literal allowed");
+        return None;
+    }
+    Some(regex)
 }
